b9ffc8ee5a14811f08a0c87970235a91994e1e7e
sarevok47 <sarevok47@proton.me>
2025-06-29 16:37:18 +0400

    Add files via upload

---
+++ script.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import re
+from openpyxl import Workbook
+from openpyxl.styles import Font
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+
+driver = webdriver.Chrome()
+driver.get('https://www.imdb.com/chart/toptv')
+html = driver.page_source
+driver.quit()
+
+wb = Workbook()
+ws = wb.active
+ws.title = "IMDB top movies"
+
+headers = ["Name", "Rating"]
+ws.append(headers)
+ws['A1'].font = ws['B1'].font = ws['C1'].font = Font(bold=True)
+
+ws.column_dimensions['A'].width = ws.column_dimensions['B'].width = 20
+ws.column_dimensions['C'].width = 40
+
+
+soup = BeautifulSoup(html, 'html.parser')
+els = soup.find_all('li', class_='ipc-metadata-list-summary-item')
+for el in els:
+  name = re.sub(r"^\d+\.\s*", "", el.select_one("h3.ipc-title__text").text)
+  rating = el.select_one("span.ipc-rating-star--rating").text
+  ws.append([name, rating])
+wb.save("imdb-top-movies-chart.xlsx")
